8000a6a5a8c517a2d4136e60749d1f7228e57639
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2021-09-25 21:33:29 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace WPF_Window_Chrome
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void CloseWindow(object sender, RoutedEventArgs e)
+		{
+			FocusButton.Focus();
+			Application.Current.Shutdown();
+		}
+		private void MaximizeWindow(object sender, RoutedEventArgs e)
+		{
+			FocusButton.Focus();
+			switch (WindowState)
+			{
+				case WindowState.Maximized: WindowState = WindowState.Normal; break;
+				case WindowState.Normal: WindowState = WindowState.Maximized; break;
+			}
+		}
+		private void MinimizeWindow(object sender, RoutedEventArgs e)
+		{
+			FocusButton.Focus();
+			WindowState = WindowState.Minimized;
+		}
+	}
+}
